0a98d9acdbe5e7f72e7cc91c599abeb91a1563fb
David Caballero <davidcame124@gmail.com>
2023-10-07 19:55:15 -0500

    deploy 1.17

--- FriendHub/settings/base.py
+++ FriendHub/settings/base.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY_DJANGO")
 
 # Application definition
 INSTALLED_APPS = [
